d7999090315d4b6e531d8762a5f79fa53ceecfb6
RKS-BlueSky <60184677+RKS-BlueSky@users.noreply.github.com>
2023-03-12 18:52:53 +0100

    first page

--- app/src/main/java/com/example/evalmobile/MainActivity.kt
+++ app/src/main/java/com/example/evalmobile/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.evalmobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.example.evalmobile.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setHeaderTitle(getString(R.string.main_title))
+
     }
 }
