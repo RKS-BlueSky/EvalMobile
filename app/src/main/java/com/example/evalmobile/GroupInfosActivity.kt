767e1d924ac42bb060c7c8ba9e64c46b6db83b95
RKS-BlueSky <60184677+RKS-BlueSky@users.noreply.github.com>
2023-03-12 20:18:14 +0100

    info page

---
+++ app/src/main/java/com/example/evalmobile/GroupInfosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.evalmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GroupInfosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_group_infos2)
+    }
+}
